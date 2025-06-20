b1c706bfdd66e7cc9b39634ff9266ad5912b61e3
Cauã <caualeu2016@gmail.com>
2025-06-20 19:25:05 -0300

    adiciona rota para gerenciar requisições de chat

---
+++ chat-api-service/src/routes/chat.routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const chatController = require('../controllers/chat.controller');
+
+const router = Router();
+
+
+router.post('/chat', chatController.handleChatRequest);
+
+module.exports = router;
