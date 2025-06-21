1f6bdee8c40f6c0f7f37e4e315c60e6eb9097f31
Cauã <caualeu2016@gmail.com>
2025-06-21 00:58:50 -0300

    refatora a estrutura do arquivo index.js, organizando as importações e melhorando a legibilidade

--- chat-api-service/index.js
+++ chat-api-service/index.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+
+// Importa as rotas
 const chatRoutes = require('./src/routes/chat.routes');
+const uploadRoutes = require('./src/routes/upload.routes');
+
+// Importa a configuração e o serviço de RAG
 const config = require('./src/config');
-const ragService = require('./src/services/rag.service'); // Importa o serviço
+const ragService = require('./src/services/rag.service'); 
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
-
-// Servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas da API
 app.use('/api', chatRoutes);
+app.use('/api', uploadRoutes);
 
 // Inicialização do Servidor
 app.listen(config.port, () => {
     console.log(`Servidor rodando na porta ${config.port}`);
     console.log(`Acesse o chat em http://localhost:${config.port}`);
-    
+    console.log(`Acesse a página de admin em http://localhost:${config.port}/admin.html`);
     ragService.startHeartbeat();
 });
